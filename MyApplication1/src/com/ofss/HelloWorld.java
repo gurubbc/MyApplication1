68f7466f1a25150ffc190f12aff17ddcf858e93d
gurubbc <gurumurthy.ramamurthy@gmail.com>
2019-04-15 20:19:33 +0530

    Updated 2

--- MyApplication1/src/com/ofss/HelloWorld.java
+++ MyApplication1/src/com/ofss/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 	System.out.println("Going to push to repository");
-	System.out.println("From Github");
+	System.out.println("From Github1");
 
 	}
 
